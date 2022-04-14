20fc7f7e0c36c9ef0f1851b3d9a59d183ca94bc2
Pierre-Eloi <pe.ragetly@gmail.com>
2022-04-14 10:27:23 +0200

    add AWS S3 conection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@
 
 import streamlit as st
 import streamlit.components.v1 as components
+import s3fs
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,3 +20,17 @@ st.title('Python Real Time Scoring API with Home Credit data')
 # description and instructions
 st.write("""A real time scoring API to predict clients' repayment abilities""")
 st.write(st.session_state)
+
+# Create connection object.
+# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+fs = s3fs.S3FileSystem(anon=False)
+@st.experimental_memo # replace @st.cache, do refer to the doc for clarifications
+def load_data(filename):
+    with fs.open(filename) as f:
+        return (pd.read_parquet(f)
+                  .set_index('SK_ID_CURR'))
+
+data_load_state = st.text('Loading data...')
+data = load_data("oc-project7-per/datasets/application_test.pqt")
+data_load_state.text("Loading data...completed")
+st.dataframe(data.head())
