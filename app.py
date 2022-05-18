a185a0d257543cf38c5598e9824cf91011fc9a57
Pierre-Eloi <pe.ragetly@gmail.com>
2022-05-18 18:00:55 +0200

    Correct predictions api

--- app.py
+++ app.py
@@ -3,16 +3,33 @@
 
 from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
 import pickle
 import json
+#import shap
 
 # read the model pickle file
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
-transformer = model['transformer']
-features_selected = model['features_selected']
-classifier = model['classifier']
+feature_names = model['features_selected']
+clf = model['classifier']
+
+# Create a predictor returning a 90% Recall
+def score_predictor(X):
+    """Predict a score with a 90% Recall.
+    -----------
+    Parameters:
+    X: array-like
+        The data to predict
+    -----------
+    Return:
+        predicted score
+    """
+    return clf.predict_proba(X)[:, 1] > 0.2
+
+# read pickle files
+# The SHAP explainer is based on the predictor above
+# with open('explainer.pkl', 'rb') as exp_file:
+#     explainer = pickle.load(exp_file)
 
 app = Flask(__name__)
 
@@ -21,20 +38,24 @@ def hello():
     # A welcome message to test the api
     return "<h1>Welcome to my prediction api!</h1>"
 
-@app.route('/api/', methods=['POST'])
-def get_prediction():
-    # read the real time input
-    json_data = request.get_json()
-    # convert json data to dictionnary
-    dict_data = json.loads(json_data)
-    # convert to pandas df
-    data = pd.DataFrame(dict_data)
+@app.route('/predictions/', methods=['POST'])
+def get_predictions():
+    content = request.get_json()
+    data = pd.DataFrame(json.loads(content))
+    # get the right features order
+    data = data[feature_names]
     # get predictions
-    data_tr = pd.DataFrame(transformer.transform(data),
-                           columns=features_selected,
-                           index=data.index)
-    data_tr['probability'] = classifier.predict_proba(data_tr)[:, 1]
-    return jsonify(data_tr.to_dict())
+    data['probability'] = clf.predict_proba(data.values)[:, 1]
+    data['score'] = score_predictor(data.drop(columns='probability').values)
+    return jsonify(data.to_dict())
+
+# @app.route('/explainer/', methods=['POST'])
+# def explain_predictions():
+#     content = request.get_json()
+#     data = pd.DataFrame(json.loads(content))
+#     shap_values = explainer.shap_values(data)
+#     f_plot = shap.force_plot(explainer.expected_value, shap_values, data)
+#     return jsonify(f_plot, shap_values)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
