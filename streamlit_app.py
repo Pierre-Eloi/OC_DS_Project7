f0998b66735637f852f5e54df510d49d11eab2a9
Pierre-Eloi <pe.ragetly@gmail.com>
2022-05-19 15:55:57 +0200

    Create two functions: get_predictions & get_shap_values. Reorganize all the script.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,80 +13,91 @@ import seaborn as sns
 import pickle
 import shap
 
-# Heroku API
-url = 'https://oc-p7-per.herokuapp.com'
 
+# -------VARIABLE DECLERATION-------
+
+# Heroku API
+#url = 'https://oc-p7-per.herokuapp.com'
+url = 'http://127.0.0.1:5000'
+# Create connection object.
+fs = s3fs.S3FileSystem(anon=False) # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+data_path = "oc-project7-per/datasets/application_test.pqt"
 # read the model pickle file
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 transformer = model['transformer']
 feature_names = model['features_selected']
-classifier = model['classifier']
 
 
-st.set_page_config(layout="wide")
-st.title('Python Real Time Scoring API with Home Credit data')
-st.write("""A real time scoring API to predict clients' repayment abilities""")
+# -------FUNCTIONS-------
 
-# Create connection object.
-# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-fs = s3fs.S3FileSystem(anon=False)
 @st.experimental_memo # replace @st.cache, do refer to the doc for clarifications
-def load_data(filename):
-    with fs.open(filename) as f:
+def load_data(file_path):
+    """Function to load data in parquet format from a s3 bucket"
+     -----------
+    Parameters:
+    filepath: string
+        data in json format
+    -----------
+    Return:
+        a DataFrame with data processed
+    """
+    with fs.open(file_path) as f:
         raw_data = (pd.read_parquet(f)
                       .set_index('SK_ID_CURR'))
         return pd.DataFrame(transformer.transform(raw_data),
                             columns=feature_names,
                             index=raw_data.index)
 
-data_load_state = st.text('Loading data...')
-data = load_data("oc-project7-per/datasets/application_test.pqt")
-data_load_state.text("Loading data...completed")
-
-# Let the user enter the client's id
-if 'get_results' not in st.session_state:
-    st.session_state['get_results'] = False
-def reinit_results():
-    st.session_state['get_results'] = False
-st.subheader("Client's ID")
-client_id = st.number_input('Insert a client ID',min_value = 100001,
-                            max_value=500000, on_change=reinit_results)
-client_data = data.loc[[client_id]]
-
-# Create a predictor returning a 90% Recall
 @st.experimental_memo
-def score_predictor(df):
-    """Predict a score with a 90% Recall.
-    -----------
+def get_predictions(json_data):
+    """Function to get prediction using POST request.
+     -----------
     Parameters:
-    df: dataframe
-        The data to predict
+    json_data: JSON string
+        data in json format
     -----------
     Return:
-        predicted score
+        a tuple with score & probability
     """
-    return df['probability'] > 0.2
+    endpoint = '/predictions/'
+    response = requests.post(f'{url}{endpoint}', json=json_data)
+    if response.status_code != 200:
+        return None
+    else:
+        return json.loads(response.content)                          
 
-# read pickle files
-# The SHAP explainer is based on the predictor above
-with open('explainer.pkl', 'rb') as file:
-    explainer = pickle.load(file)
+@st.experimental_memo
+def get_shap_values(json_data):
+    """function to get shap values using POST rsquest
+     -----------
+    Parameters:
+    json_data: JSON string
+        data in json format
+    -----------
+    Return:
+        a tuple with shap values & expected value
+    """
+    endpoint = '/shap/'
+    response = requests.post(f'{url}{endpoint}', json=json_data)
+    if response.status_code != 200:
+        
+        return None
+    else:
+        shap_values, exp_value = json.loads(response.content)
+        shap_values = np.array(shap_values).reshape(1, -1)
+        return shap_values, exp_value
+
+# Reinit the session state when the user enters a new client
+def reinit_results():
+    st.session_state['get_results'] = False
 
 # Plot a force plot in streamlit API
 def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
-# Explain model prediction results with a force plot
-@st.experimental_memo
-def explain_model(X):
-    # Calculate Shap values
-    shap_values = explainer.shap_values(X)
-    f_plot = shap.force_plot(explainer.expected_value, shap_values, X)
-    return f_plot, shap_values
-
-# Get the list of top features
+# Get the top features list
 def top_features(shap_values, feature_names, max_display=10):
     """Sort features by importance and display the top ones.
     -----------
@@ -108,6 +119,28 @@ def top_features(shap_values, feature_names, max_display=10):
               .to_list())
 
 
+# -------DASHBOARD-------
+
+st.set_page_config(layout="wide")
+
+st.title('Python Real Time Scoring API with Home Credit data')
+st.write("""A real time scoring API to predict clients' repayment abilities""")
+
+# load data
+data_load_state = st.text('Loading data...')
+data = load_data(data_path)
+data_load_state.text("Loading data...completed")
+
+# Let the user enter the client's id
+st.subheader("Client's ID")
+client_id = st.number_input('Insert a client ID',min_value = 100001,
+                            max_value=500000, on_change=reinit_results)
+client_data = data.loc[[client_id]]
+json_data = client_data.to_json()
+
+if 'get_results' not in st.session_state:
+    st.session_state['get_results'] = False
+
 # To automatically display results when changing a widget value
 if st.button('Get predictions'):
     st.session_state['get_results'] = True
@@ -115,42 +148,34 @@ if st.button('Get predictions'):
 # Display results
 if st.session_state['get_results']:
     st.header('Results')
-    # Get Predictions
-    json_data = client_data.to_json()
-    endpoint = '/predictions/'
     pred_state = st.text("Waiting for predictions...")
-    response = requests.post(f'{url}{endpoint}', json=json_data)
-    if response.status_code != 200:
-        pred_state.text("error: the prediction API request did not work")
+    score, probability = get_predictions(json_data)
+    if score is None:
+        pred_state.text("error: the prediction API request did not work") 
     else:
         pred_state.text("predictions successfully loaded")
-        client_data = pd.DataFrame(json.loads(response.content))
-        score = int(client_data['score'])
-        probability = round(float(client_data['probability']), 2)
-    st.write('**Client:**', client_id)
-    st.write('**Score:**', score)
-    st.write('**probability:**', probability)
+        st.write('**Client:**', client_id)
+        st.write('**Score:**', score)
+        st.write('**probability:**', probability)  
     if score == 1:
-        message = '<span style="font-size:20px; color:red">\
+        message = '<span style="font-size:18px; color:red">\
                     The client will probably be in **default**.</span>'
     else:
-        message = '<span style="font-size:20px; color:green">\
+        message = '<span style="font-size:18px; color:green">\
                    The Client\'s default is **unlikely**</span>'
     st.markdown(message, unsafe_allow_html=True)
+
     # laying out the middle section of the app
     col_1, col_2 = st.columns((1, 1))
     with col_1:
-        # SHAP force plot
         st.subheader('Prediction Interpretation Plot')
         # Get shap values
-        endpoint = '/explainer/'
-        response = requests.post(f'{url}{endpoint}', json=json_data)
-        if response.status_code != 200:
-            st.text("error: the explainer API request did not work")
+        shap_values, exp_value = get_shap_values(json_data)
+        if shap_values is None:
+            st.text("error: the explainer API request did not work") 
         else:
-            p, shap_values = json.loads(response.content)
-        st_shap(p)
-        # Summary plot
+            f_plot = shap.force_plot(exp_value, shap_values, client_data)
+            st_shap(f_plot)
         st.subheader('Summary Plot')
         fig = plt.figure()
         shap.summary_plot(shap_values, client_data, max_display=10, show=False)
