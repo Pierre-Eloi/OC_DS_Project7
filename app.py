9e0a35a92359fcb3818baf210658344f33ef45da
Pierre-Eloi <pe.ragetly@gmail.com>
2022-05-20 16:10:18 +0200

    add score_predictor function & explainer.pkl import to test heroku deployment

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ with open('model.pkl', 'rb') as model_file:
 feature_names = model['features_selected']
 clf = model['classifier']
 
+# Create a predictor returning a 90% Recall
+def score_predictor(X):
+    """Predict a score with a 90% Recall.
+    -----------
+    Parameters:
+    X: array-like
+        The data to predict
+    -----------
+    Return:
+        predicted score
+    """
+    return clf.predict_proba(X)[:, 1] > 0.2
 
+# read pickle files
+# The SHAP explainer is based on the predictor above
+with open('explainer.pkl', 'rb') as exp_file:
+    explainer = pickle.load(exp_file)
 
 app = Flask(__name__)
 
