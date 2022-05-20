774df012cb576036a51e1a94996343f4ef08631a
Pierre-Eloi <pe.ragetly@gmail.com>
2022-05-20 16:04:54 +0200

    add model.pkl import to test heroku deployment

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ import pickle
 import json
 import shap
 
+# read the model pickle file
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+feature_names = model['features_selected']
+clf = model['classifier']
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
